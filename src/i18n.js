daa096030725d9b01ec6ea2c5127e3e17e607a7f
kokolem <vitek.peterk@gmail.com>
2021-05-04 23:15:17 +0200

    Allow language detection from url and navigator

--- src/i18n.js
+++ src/i18n.js
@@ -3,16 +3,31 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+i18n.on('languageChanged', (lng) => {
+  const urlLang = window.location.pathname.match(/\/([a-zA-Z-]*)/g)[process.env.REACT_APP_LANG_PATH_INDEX]
+    .replace('/', '');
+  if (urlLang !== lng) {
+    window.history.replaceState({}, '', lng);
+  }
+
+  document.title = i18n.t('Page title');
+  document.documentElement.lang = lng;
+});
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    supportedLngs: ['cs', 'de'],
     fallbackLng: {
-      sk: ['cs'],
-      default: ['cs'],
+      default: [process.env.REACT_APP_DEFAULT_LANG],
     },
     debug: process.env.NODE_ENV === 'development',
+    detection: {
+      order: ['path', 'navigator'],
+      lookupFromPathIndex: process.env.REACT_APP_LANG_PATH_INDEX,
+    },
   });
 
 export default i18n;
